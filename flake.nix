ca881830bddac37c53d2f20aa651e6f5a82f1d56
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-07-09 20:03:49 +0800

    feat: Initialize nix env

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A rust devShell";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            eza
+            fd
+            alsa-lib
+
+            rust-bin.beta.latest.default
+          ];
+
+          shellHook = ''
+            alias ls=exa
+            alias find=fd
+            export RUST_BACKTRACE=1
+          '';
+        };
+      }
+    );
+}
