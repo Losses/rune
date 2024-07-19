32ad00019ef645feafa8403b8ac79996e977265c
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-07-19 22:10:32 +0800

    feat: Add flutter development environment

--- flake.nix
+++ flake.nix
@@ -1,24 +1,83 @@
 {
-  description = "A rust devShell";
+  description = "A combined Flutter and Rust devShell";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    android-nixpkgs = {
+      url = "github:tadfisher/android-nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, flake-compat, android-nixpkgs, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
+          config = {
+            allowUnfree = true;
+            android_sdk = {
+              accept_license = true;
+            };
+          };
         };
-      in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          buildInputs = [
+
+        androidEnvCustomPackage = pkgs.androidenv.composeAndroidPackages {
+          toolsVersion = "26.1.1";
+          platformToolsVersion = "34.0.5";
+          buildToolsVersions = [ "30.0.3" "34.0.0" ];
+          includeEmulator = true;
+          emulatorVersion = "34.1.9";
+          platformVersions = [ "28" "29" "30" "31" "32" "33" "34" ];
+          includeSources = false;
+          includeSystemImages = false;
+          systemImageTypes = [ "google_apis_playstore" ];
+          abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+          cmakeVersions = [ "3.10.2" ];
+          includeNDK = true;
+          ndkVersions = [ "22.0.7026061" ];
+          useGoogleAPIs = false;
+          useGoogleTVAddOns = false;
+        };
+
+        androidCustomPackage = android-nixpkgs.sdk.${system} (
+          sdkPkgs: with sdkPkgs; [
+            cmdline-tools-latest
+            build-tools-30-0-3
+            build-tools-33-0-2
+            build-tools-34-0-0
+            platform-tools
+            emulator
+            platforms-android-28
+            platforms-android-29
+            platforms-android-30
+            platforms-android-31
+            platforms-android-32
+            platforms-android-33
+            platforms-android-34
+          ]
+        );
+
+        pinnedJDK = pkgs.jdk17;
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "Combined Flutter and Rust Dev Shell";
+          buildInputs = with pkgs; [
+            flutter
+            android-studio
+            rust-bin.beta.latest.default
             openssl
             pkg-config
             eza
@@ -26,13 +85,19 @@
             alsa-lib
             libpulseaudio
             pulseaudioFull
-            rust-bin.beta.latest.default
+          ] ++ [
+            pinnedJDK
+            androidCustomPackage
           ];
 
           shellHook = ''
             alias ls=exa
             alias find=fd
             export RUST_BACKTRACE=1
+            JAVA_HOME=${pinnedJDK}
+            ANDROID_HOME=${androidCustomPackage}/share/android-sdk
+            GRADLE_USER_HOME=/home/specx/.gradle
+            GRADLE_OPTS="-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidCustomPackage}/share/android-sdk/build-tools/34.0.0/aapt2"
           '';
         };
       }
