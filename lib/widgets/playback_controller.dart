0c94821d4dd3594fba454343f6eb3e1351e24410
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-07-25 22:11:23 +0800

    feat: Implement a progress indicator

--- lib/widgets/playback_controller.dart
+++ lib/widgets/playback_controller.dart
@@ -2,6 +2,18 @@ import 'package:fluent_ui/fluent_ui.dart';
 
 import '../messages/playback.pb.dart';
 
+String formatTime(double seconds) {
+  int totalSeconds = seconds.floor();
+
+  int minutes = totalSeconds ~/ 60;
+  int remainingSeconds = totalSeconds % 60;
+
+  String minutesStr = minutes.toString().padLeft(2, '0');
+  String secondsStr = remainingSeconds.toString().padLeft(2, '0');
+
+  return '$minutesStr:$secondsStr';
+}
+
 class PlaybackController extends StatefulWidget {
   const PlaybackController({super.key});
 
@@ -23,21 +35,29 @@ class PlaybackControllerState extends State<PlaybackController> {
         final state = playbackStatus.state;
         final progressSeconds = playbackStatus.progressSeconds;
         final progressPercentage = playbackStatus.progressPercentage;
-        final artist = playbackStatus.artist;
-        final album = playbackStatus.album;
         final title = playbackStatus.title;
         final duration = playbackStatus.duration;
 
         return Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Column(
+              children: [
+                Text(title),
+                Slider(
+                  value: progressPercentage * 100,
+                  onChanged: (v) => print(v),
+                  style: const SliderThemeData(useThumbBall: false),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(formatTime(progressSeconds)),
+                    Text('-${formatTime(duration - progressSeconds)}'),
+                  ],
+                )
+              ],
+            ),
             Text("Status: $state"),
-            Text(
-                "Progress: ${progressSeconds.toStringAsFixed(2)} seconds (${(progressPercentage * 100).toStringAsFixed(2)}%)"),
-            Text("Artist: $artist"),
-            Text("Album: $album"),
-            Text("Title: $title"),
-            Text("Duration: ${duration.toStringAsFixed(2)} seconds"),
           ],
         );
       },
