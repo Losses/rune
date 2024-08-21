367805ed8817edca4efc2271a6d485596049b477
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-08-21 08:53:55 +0800

    refactor: Performance improvement

--- lib/widgets/flip_tile.dart
+++ lib/widgets/flip_tile.dart
@@ -1,9 +1,10 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_boring_avatars/flutter_boring_avatars.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 import 'flip_grid.dart';
 
-class FlipTile extends StatelessWidget {
+class FlipTile extends StatefulWidget {
   final String name;
   final List<int> coverIds;
   final VoidCallback onPressed;
@@ -17,42 +18,69 @@ class FlipTile extends StatelessWidget {
       this.emptyTileType = BoringAvatarsType.bauhaus});
 
   @override
+  FlipTileState createState() => FlipTileState();
+}
+
+class FlipTileState extends State<FlipTile> {
+  bool _isVisible = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Button(
-      style:
-          const ButtonStyle(padding: WidgetStatePropertyAll(EdgeInsets.all(0))),
-      onPressed: onPressed,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(3),
-        child: SizedBox(
-          width: double.infinity,
-          height: double.infinity,
-          child: Stack(
-            alignment: Alignment.bottomLeft,
-            children: [
-              FlipCoverGrid(
-                  numbers: coverIds, id: name, emptyTileType: emptyTileType),
-              Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: const Alignment(0.0, -1.0),
-                    end: const Alignment(0.0, 1.0),
-                    colors: [
-                      Colors.black.withAlpha(0),
-                      Colors.black.withAlpha(160),
+    return VisibilityDetector(
+      key: Key(widget.name),
+      onVisibilityChanged: (VisibilityInfo info) {
+        if (!mounted) return;
+        if (info.visibleFraction > 0.01) {
+          setState(() {
+            _isVisible = true;
+          });
+        } else {
+          setState(() {
+            _isVisible = false;
+          });
+        }
+      },
+      child: Button(
+        style: const ButtonStyle(
+            padding: WidgetStatePropertyAll(EdgeInsets.all(0))),
+        onPressed: widget.onPressed,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(3),
+          child: SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: _isVisible
+                ? Stack(
+                    alignment: Alignment.bottomLeft,
+                    children: [
+                      FlipCoverGrid(
+                        numbers: widget.coverIds,
+                        id: widget.name,
+                        emptyTileType: widget.emptyTileType,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: const Alignment(0.0, -1.0),
+                            end: const Alignment(0.0, 1.0),
+                            colors: [
+                              Colors.black.withAlpha(0),
+                              Colors.black.withAlpha(160),
+                            ],
+                          ),
+                        ),
+                        height: 80,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: Text(
+                          widget.name,
+                          textAlign: TextAlign.start,
+                        ),
+                      ),
                     ],
-                  ),
-                ),
-                height: 80,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(6),
-                child: Text(
-                  name,
-                  textAlign: TextAlign.start,
-                ),
-              ),
-            ],
+                  )
+                : const SizedBox.expand(),
           ),
         ),
       ),
