882fb0a74a1f59aa4bd1995110c35e2ff7cb4468
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-09-04 19:48:26 +0800

    feat: Adapt animation for query tracks list

--- lib/screens/query_tracks/query_tracks.dart
+++ lib/screens/query_tracks/query_tracks.dart
@@ -1,10 +1,17 @@
+import 'dart:async';
+
+import 'package:provider/provider.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:go_router/go_router.dart';
-import 'package:player/utils/router_extra.dart';
+
+import '../../utils/router_extra.dart';
+
+import '../../config/animation.dart';
 
 import '../../screens/query_tracks/widgets/query_tracks.dart';
 
 import '../../widgets/playback_controller.dart';
+import '../../widgets/start_screen/providers/start_screen_layout_manager.dart';
 
 class QueryTracksPage extends StatefulWidget {
   final List<int> artistIds;
@@ -22,31 +29,43 @@ class QueryTracksPage extends StatefulWidget {
 }
 
 class _QueryTracksPageState extends State<QueryTracksPage> {
+  final _layoutManager = StartScreenLayoutManager();
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(milliseconds: gridAnimationDelay),
+        () => _layoutManager.playAnimations());
+  }
+
   @override
   Widget build(BuildContext context) {
     final FluentThemeData theme = FluentTheme.of(context);
     final Typography typography = theme.typography;
     final extra = GoRouterState.of(context).extra;
 
-    return ScaffoldPage(
-      header: HyperlinkButton(
-        style: ButtonStyle(
-          textStyle: WidgetStateProperty.all(typography.title),
-        ),
-        child: Text(extra is QueryTracksExtra ? extra.title : 'Tracks',
-            style: TextStyle(color: theme.inactiveColor)),
-        onPressed: () => {context.pop()},
-      ),
-      content: Column(children: [
-        Expanded(
-          child: QueryTrackListView(
-            artistIds: widget.artistIds,
-            albumIds: widget.albumIds,
-            playlistIds: widget.playlistIds,
+    return ChangeNotifierProvider<StartScreenLayoutManager>.value(
+        value: _layoutManager,
+        child: ScaffoldPage(
+          header: HyperlinkButton(
+            style: ButtonStyle(
+              textStyle: WidgetStateProperty.all(typography.title),
+            ),
+            child: Text(extra is QueryTracksExtra ? extra.title : 'Tracks',
+                style: TextStyle(color: theme.inactiveColor)),
+            onPressed: () => {context.pop()},
           ),
-        ),
-        const PlaybackPlaceholder(),
-      ]),
-    );
+          content: Column(children: [
+            Expanded(
+              child: QueryTrackListView(
+                artistIds: widget.artistIds,
+                albumIds: widget.albumIds,
+                playlistIds: widget.playlistIds,
+              ),
+            ),
+            const PlaybackPlaceholder(),
+          ]),
+        ));
   }
 }
