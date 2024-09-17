54fed89302acf1fb337d3fff5d427b18797521ab
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-09-17 11:22:27 +0800

    fix: Building issue

--- flake.nix
+++ flake.nix
@@ -85,29 +85,30 @@
             (rust-bin.stable.latest.default.override {
               extensions = [ "rust-src" ];
             })
+            openssl
+            pkg-config
+            stdenv
             clippy
             rust-analyzer
             rustup
-            pkgs.buildPackages.pkg-config
             expidusPkgs.flutter
             android-studio
             androidCustomPackage
             pinnedJDK
             clang
-            libstdcxx5
             cmake
             protobuf_26
             pcre2
             mount
             ninja
             unzip
+            wayland
           ];
 
           buildInputs = with pkgs; [
             eza
             fd
             gtk3
-            alsa-lib
             libpulseaudio
             pulseaudioFull
             fontconfig
@@ -115,11 +116,11 @@
             libxkbcommon
             pkgs.xorg.libX11
             libGL
+            alsa-lib.dev
             wayland.dev
             zstd.dev
             lmdb.dev
             sqlite.dev
-            openssl.dev
           ];
 
           shellHook = ''
@@ -130,13 +131,10 @@
             export JAVA_HOME=${pinnedJDK}
             export ANDROID_HOME=${androidCustomPackage}/share/android-sdk
             export GRADLE_USER_HOME=/home/specx/.gradle
-            export ZSTD_SYS_USE_PKG_CONFIG=1
-            export LIBSQLITE3_SYS_USE_PKG_CONFIG=1
-            export PKG_CONFIG_ALLOW_CROSS=1
-            export PKG_CONFIG_PATH=${pkgs.zstd.dev}/lib/pkgconfig:${pkgs.lmdb.dev}/lib/pkgconfig:${pkgs.sqlite.dev}/lib/pkgconfig:${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.alsa-lib.dev}/lib/pkgconfig
             export LD_LIBRARY_PATH=$(nix-build '<nixpkgs>' -A wayland)/lib:${pkgs.fontconfig.lib}/lib:${pkgs.libxkbcommon}/lib:${pkgs.xorg.libX11}/lib:${pkgs.libGL}/lib:$LD_LIBRARY_PATH
             export GRADLE_OPTS="-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidCustomPackage}/share/android-sdk/build-tools/34.0.0/aapt2"
             export PATH=${androidCustomPackage}/share/android-sdk/platform-tools:${androidCustomPackage}/share/android-sdk/tools:${androidCustomPackage}/share/android-sdk/tools/bin:$HOME/.cargo/bin:$HOME/.pub-cache/bin:$PATH
+            echo ${pkgs.openssl.dev}
           '';
         };
 
