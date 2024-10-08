e394ac27d329417b909acf2dc61ee6866b5b54b6
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-10-08 10:57:44 +0800

    refactor: Improve performance by manually implement a button instead the on in `fluent_ui`

--- lib/widgets/tile/tile.dart
+++ lib/widgets/tile/tile.dart
@@ -1,6 +1,6 @@
 import 'package:fluent_ui/fluent_ui.dart';
 
-class Tile extends StatelessWidget {
+class Tile extends StatefulWidget {
   const Tile({
     super.key,
     required this.onPressed,
@@ -11,18 +11,58 @@ class Tile extends StatelessWidget {
   final Widget child;
 
   @override
+  TileState createState() => TileState();
+}
+
+class TileState extends State<Tile> {
+  bool _isHovered = false;
+  final FocusNode _focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Button(
-      style: const ButtonStyle(
-        padding: WidgetStatePropertyAll(
-          EdgeInsets.all(0),
-        ),
-      ),
-      onPressed: onPressed,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(3),
-        child: SizedBox.expand(
-          child: child,
+    final theme = FluentTheme.of(context);
+    Color borderColor;
+
+    if (_isHovered || _focusNode.hasFocus) {
+      borderColor = theme.resources.controlStrokeColorDefault;
+    } else {
+      borderColor = theme.resources.controlStrokeColorSecondary;
+    }
+
+    return GestureDetector(
+      onTap: widget.onPressed,
+      child: MouseRegion(
+        onEnter: (_) => setState(() => _isHovered = true),
+        onExit: (_) => setState(() => _isHovered = false),
+        child: FocusableActionDetector(
+          focusNode: _focusNode,
+          child: AnimatedContainer(
+            duration: theme.fastAnimationDuration,
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              border: Border.all(color: borderColor),
+              borderRadius: BorderRadius.circular(4),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(3),
+              child: widget.child,
+            ),
+          ),
         ),
       ),
     );
