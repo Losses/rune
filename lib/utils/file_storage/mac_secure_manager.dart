f6028683bada4b1eba8442e4353ddb9fd1b53cd9
XMLHexagram <52130356+XMLHexagram@users.noreply.github.com>
2024-10-09 16:26:12 +0200

    feat: bring sandbox back and add MacSecureManager (#13)
    * feat: bring sandbox back and add MacSecureManager
    
    * chroe: delete useless debug print

---
+++ lib/utils/file_storage/mac_secure_manager.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+import 'package:macos_secure_bookmarks/macos_secure_bookmarks.dart';
+
+import 'dart:io' show Platform;
+
+import 'package:get_storage/get_storage.dart';
+
+class MacSecureManager {
+  static final MacSecureManager shared = MacSecureManager();
+  static const String storageName = 'mac_secure_manager';
+
+  final GetStorage _storage = GetStorage(storageName);
+
+  static isApplePlatform() {
+    return Platform.isMacOS || Platform.isIOS;
+  }
+
+  Future<void> saveBookmark(String dir) async {
+    if (!isApplePlatform()) {
+      return;
+    }
+    final secureBookmarks = SecureBookmarks();
+    final bookmark = await secureBookmarks.bookmark(Directory(dir));
+    await _storage.write(dir, bookmark);
+  }
+
+  Future<void> loadBookmark() async {
+    if (!isApplePlatform()) {
+      return;
+    }
+    final secureBookmarks = SecureBookmarks();
+
+    final bookmarks = _storage.getValues<Iterable<dynamic>>().toList();
+    
+    for (final bookmark in bookmarks) {
+      final resolvedFile = await secureBookmarks.resolveBookmark(bookmark, isDirectory: true);
+      await secureBookmarks.startAccessingSecurityScopedResource(resolvedFile);
+    }
+  }
+}
