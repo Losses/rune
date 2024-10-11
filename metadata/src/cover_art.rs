b894f2e782f4c9ee9a92dfb4d24d061f1204b7b9
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-10-11 12:47:35 +0800

    feat: Calculate CRC based on cover art content but not the file hash

--- metadata/src/cover_art.rs
+++ metadata/src/cover_art.rs
@@ -1,11 +1,36 @@
-use lofty::file::TaggedFileExt;
 use std::path::Path;
+
+use anyhow::Result;
+use image::{GenericImageView, ImageBuffer, Pixel};
+use lofty::file::TaggedFileExt;
 
 use crate::crc::media_crc32;
 
 pub struct CoverArt {
     pub crc: String,
     pub data: Vec<u8>,
+}
+
+fn decode_image(image_data: &Vec<u8>) -> Result<Vec<u8>> {
+    // Decode the image from binary data
+    let img = image::load_from_memory(&image_data)?;
+
+    // Resize the image to 16x16 pixels
+    let resized = img.resize_exact(16, 16, image::imageops::FilterType::Lanczos3);
+
+    // Create a new RGB image buffer
+    let mut rgb_image = ImageBuffer::new(16, 16);
+
+    // Copy the resized image into the RGB buffer
+    for (x, y, pixel) in resized.pixels() {
+        let rgb = pixel.to_rgb();
+        rgb_image.put_pixel(x, y, rgb);
+    }
+
+    // Convert the RGB image into a flat RGB sequence
+    let rgb_sequence: Vec<u8> = rgb_image.into_raw();
+
+    Ok(rgb_sequence)
 }
 
 pub fn extract_cover_art_binary(file_path: &Path) -> Option<CoverArt> {
@@ -27,8 +52,10 @@ pub fn extract_cover_art_binary(file_path: &Path) -> Option<CoverArt> {
         return None;
     }
 
+    let rgb_sequence = decode_image(&cover_data).ok()?;
+
     // Calculate the CRC
-    let crc = media_crc32(&cover_data, 0, 0, cover_data.len());
+    let crc = media_crc32(&rgb_sequence, 0, 0, rgb_sequence.len());
 
     if crc == 0 {
         return None;
