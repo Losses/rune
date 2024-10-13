e56a8f15cee52146eddb02235135939095d8fc4c
Rachel Tang <13704467+Rachel030219@users.noreply.github.com>
2024-10-13 19:27:29 +0800

    fix: Android compile process (#26)
    * fix: link libc++_shared on android
    
    * fix: don't wait for windowManager on Android
    
    * fix: gradle res not found by upgrading compile SDK
    
    * fix: use dummy souvlaki on Android to pass rust build
    
    The media control on Android is unimplemented as of now, and will work,
    hopefully, in the near future.
    
    * chore: change Android package name

--- native/hub/build.rs
+++ native/hub/build.rs
@@ -1,7 +1,17 @@
+use std::env;
 use anyhow::Result;
 use vergen_git2::{BuildBuilder, Emitter, Git2Builder, RustcBuilder};
 
 fn main() -> Result<()> {
+    let target_os = env::var("CARGO_CFG_TARGET_OS");
+    match target_os.as_ref().map(|x| &**x) {
+        Ok("android") => {
+            println!("cargo:rustc-link-lib=dylib=stdc++");
+            println!("cargo:rustc-link-lib=c++_shared");
+        },
+        _ => {}
+    }
+
     let git2 = Git2Builder::all_git()?;
     let build = BuildBuilder::all_build()?;
     let rustc = RustcBuilder::all_rustc()?;
--- playback/src/lib.rs
+++ playback/src/lib.rs
@@ -1,9 +1,15 @@
 mod internal;
 mod realtime_fft;
+#[cfg(target_os = "android")]
+mod dummy_souvlaki;
 
 pub mod controller;
 pub mod player;
 
+#[cfg(not(target_os = "android"))]
 pub use souvlaki::{MediaMetadata, MediaPlayback, MediaPosition};
+
+#[cfg(target_os = "android")]
+pub use dummy_souvlaki::{MediaMetadata, MediaPlayback, MediaPosition};
 
 pub use internal::{PlayerCommand, PlayerEvent};
