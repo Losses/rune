6df6e146b6bade67496ee9350317e7760f0cb152
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-10-16 20:20:36 +0800

    chore: Code cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter/foundation.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:system_theme/system_theme.dart';
 import 'package:fluent_ui/fluent_ui.dart' hide Page;
-import 'package:window_manager/window_manager.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter_fullscreen/flutter_fullscreen.dart';
 import 'package:flutter_acrylic/flutter_acrylic.dart' as flutter_acrylic;
@@ -37,9 +36,7 @@ import 'router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (!Platform.isAndroid) {
-    await windowManager.ensureInitialized();
-  }
+
   await FullScreen.ensureInitialized();
   await GetStorage.init();
   await GetStorage.init(MacSecureManager.storageName);
@@ -137,16 +134,12 @@ class Rune extends StatelessWidget {
             brightness: Brightness.dark,
             accentColor: appTheme.color,
             visualDensity: VisualDensity.standard,
-            focusTheme: FocusThemeData(
-              glowFactor: is10footScreen(context) ? 2.0 : 0.0,
-            ),
+            focusTheme: const FocusThemeData(glowFactor: 2.0),
           ),
           theme: FluentThemeData(
             accentColor: appTheme.color,
             visualDensity: VisualDensity.standard,
-            focusTheme: FocusThemeData(
-              glowFactor: is10footScreen(context) ? 2.0 : 0.0,
-            ),
+            focusTheme: const FocusThemeData(glowFactor: 2.0),
           ),
           locale: appTheme.locale,
           routerDelegate: router.routerDelegate,
