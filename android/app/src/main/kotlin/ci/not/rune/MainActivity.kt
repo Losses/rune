1ff3c0a43a5b7f90c196c6e0c7855513878a2b21
Rachel Tang <13704467+Rachel030219@users.noreply.github.com>
2024-10-30 10:27:11 +0800

    fix: Playback on Android by passing context to Rust Audio with JNI (#57)

--- android/app/src/main/kotlin/ci/not/rune/MainActivity.kt
+++ android/app/src/main/kotlin/ci/not/rune/MainActivity.kt
@@ -1,7 +1,26 @@
 package ci.not.rune
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    
+
+    companion object {
+        init {
+            System.loadLibrary("hub")  // load Rust library
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initializeContext(this)
+    }
+
+    override fun onDestroy() {
+        releaseContext()
+        super.onDestroy()
+    }
+
+    private external fun initializeContext(context: Any)
+    private external fun releaseContext()
 }
