13dd59a1c2ed36120cc011dd34b46778b8a0cd3f
Rachel Tang <13704467+Rachel030219@users.noreply.github.com>
2024-11-05 09:49:47 +0800

    fix: Back button press event on Android (#61)

--- android/app/src/main/kotlin/ci/not/rune/MainActivity.kt
+++ android/app/src/main/kotlin/ci/not/rune/MainActivity.kt
@@ -1,9 +1,13 @@
 package ci.not.rune
 
 import android.os.Bundle
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private var popChannel: MethodChannel? = null
 
     companion object {
         init {
@@ -23,4 +27,36 @@ class MainActivity: FlutterActivity() {
 
     private external fun initializeContext(context: Any)
     private external fun releaseContext()
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        popChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "ci.not.rune/pop")
+    }
+
+    /**
+     * Though this method is marked as deprecated, it is still called by the system. And to avoid
+     * Flutter killing entire app when back button is pressed, we override this method and handle
+     * the back button press event here.
+     */
+    @Deprecated("Deprecated in newer Android versions for predictive back feature.")
+    override fun onBackPressed() {
+        // Check if the Flutter side wants to handle the back button press event
+        popChannel?.invokeMethod("pop", null, object: MethodChannel.Result {
+            override fun success(result: Any?) {
+                if (result is Boolean) {
+                    // true: Flutter side handled the back button press event, popping the route
+                    // false: Flutter side did not handle, let's go to background
+                    if (result) return else moveTaskToBack(true)
+                }
+            }
+
+            override fun error(errorCode: String, errorMessage: String?, errorDetails: Any?) {
+                Log.e("Pop", "Error: $errorCode, $errorMessage, $errorDetails")
+            }
+
+            override fun notImplemented() {
+                Log.e("Pop", "Not implemented")
+            }
+        })
+    }
 }
--- lib/widgets/shortcuts/router_actions_manager.dart
+++ lib/widgets/shortcuts/router_actions_manager.dart
@@ -1,4 +1,5 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/services.dart';
 import 'package:rune/utils/navigation/utils/navigation_backward.dart';
 
 import '../../utils/navigation/back_action.dart';
@@ -13,11 +14,19 @@ import '../navigation_mouse_key_listener.dart';
 
 class NavigationShortcutManager extends StatelessWidget {
   const NavigationShortcutManager(this.child, {super.key});
+  // Method channel for Android back button
+  static const MethodChannel popChannel = MethodChannel('ci.not.rune/pop');
 
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
+    popChannel.setMethodCallHandler((call) async {
+      // Handle back button press on Android
+      if (call.method == 'pop') {
+        return navigationBackward();
+      }
+    });
     return Actions(
       actions: <Type, Action<Intent>>{
         NavigationIntent: NavigationAction(),
