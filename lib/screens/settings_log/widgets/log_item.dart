30d07faddcdce2d5b039d55e8d9ec3d720d3d25c
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-11-18 09:28:12 +0800

    chore: Code cleanup

--- lib/screens/settings_log/widgets/log_item.dart
+++ lib/screens/settings_log/widgets/log_item.dart
@@ -1,50 +1,11 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:material_symbols_icons/material_symbols_icons.dart';
 
-import '../../../utils/l10n.dart';
 import '../../../widgets/context_menu_wrapper.dart';
+import '../../../screens/settings_log/utils/open_log_item_context_menu.dart';
 import '../../../messages/all.dart';
 
 import '../../settings_library/widgets/settings_button.dart';
-
-void openLogItemContextMenu(
-  Offset localPosition,
-  BuildContext context,
-  GlobalKey contextAttachKey,
-  FlyoutController contextController,
-  int index,
-  void Function(int) onViewDetail,
-  void Function(int) onDelete,
-) async {
-  final targetContext = contextAttachKey.currentContext;
-
-  if (targetContext == null) return;
-  final box = targetContext.findRenderObject() as RenderBox;
-  final position = box.localToGlobal(
-    localPosition,
-    ancestor: Navigator.of(context).context.findRenderObject(),
-  );
-
-  if (!context.mounted) return;
-
-  contextController.showFlyout(
-    position: position,
-    builder: (context) => MenuFlyout(
-      items: [
-        MenuFlyoutItem(
-          leading: const Icon(Symbols.visibility),
-          text: Text(S.of(context).viewLogDetail),
-          onPressed: () => onViewDetail(index),
-        ),
-        MenuFlyoutItem(
-          leading: const Icon(Symbols.delete),
-          text: Text(S.of(context).delete),
-          onPressed: () => onDelete(index),
-        ),
-      ],
-    ),
-  );
-}
 
 class LogItem extends StatefulWidget {
   final LogDetail log;
