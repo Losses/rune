c3995c85f21ae5dae92817f96a5e24389d89b5ab
Shibo Lyu <hi@lao.sb>
2024-11-22 02:18:18 +0800

    fix: init rustup after installing from brew

--- scripts/macos_1_install.sh
+++ scripts/macos_1_install.sh
@@ -19,6 +19,8 @@ install_if_missing rustup "rustup" "rustup"
 
 brew install lmdb create-dmg protobuf
 
+rustup-init -y
+. "$HOME/.cargo/env"
 rustup default stable
 rustup target add x86_64-apple-darwin
 rustup target add aarch64-apple-darwin
