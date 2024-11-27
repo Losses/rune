d5e4f5db102e69e66fa15648c74afa979634340b
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-11-27 18:38:53 +0800

    chore: Code cleanup

--- lib/screens/settings_home/large_screen_settings_home_list.dart
+++ lib/screens/settings_home/large_screen_settings_home_list.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
 
 import '../../config/animation.dart';
+import '../../utils/settings_manager.dart';
 import '../../widgets/smooth_horizontal_scroll.dart';
 import '../../widgets/start_screen/link_tile.dart';
 import '../../widgets/start_screen/start_group.dart';
@@ -12,6 +14,7 @@ import '../../widgets/start_screen/providers/start_screen_layout_manager.dart';
 import '../../screens/settings_home/constants/first_column.dart';
 import '../../widgets/navigation_bar/page_content_frame.dart';
 import '../../utils/l10n.dart';
+import '../settings_about/settings_about.dart';
 
 class LargeScreenSettingsHomeListView extends StatefulWidget {
   const LargeScreenSettingsHomeListView({
@@ -30,6 +33,8 @@ class LargeScreenSettingsHomeListView extends StatefulWidget {
 
 class LargeScreenSettingsHomeListViewState
     extends State<LargeScreenSettingsHomeListView> {
+  bool? mysterious;
+
   @override
   void initState() {
     super.initState();
@@ -37,6 +42,14 @@ class LargeScreenSettingsHomeListViewState
       Duration(milliseconds: gridAnimationDelay),
       () => widget.layoutManager.playAnimations(),
     );
+
+    SettingsManager().getValue<bool>(mysteriousKey).then((x) {
+      if (!context.mounted) return;
+
+      setState(() {
+        mysterious = x;
+      });
+    });
   }
 
   @override
@@ -65,7 +78,6 @@ class LargeScreenSettingsHomeListViewState
                     dimensionCalculator:
                         StartGroupImplementation.startLinkDimensionCalculator,
                     gapSize: defaultGapSize,
-                    onTitleTap: () {},
                     itemBuilder: (context, item) {
                       return LinkTile(
                         title: item.$1(context),
@@ -75,6 +87,41 @@ class LargeScreenSettingsHomeListViewState
                     },
                     direction: Axis.horizontal,
                   ),
+                  if (mysterious == true)
+                    StartGroup<
+                        (
+                          String Function(BuildContext),
+                          String,
+                          IconData,
+                          bool
+                        )>(
+                      groupIndex: 1,
+                      groupTitle: S.of(context).mysteriousModalTitle,
+                      items: [
+                        (
+                          (context) => S.of(context).laboratory,
+                          '/settings/library',
+                          Symbols.interests,
+                          false
+                        ),
+                      ],
+                      constraints: constraints,
+                      groupLayoutVariation:
+                          StartGroupGroupLayoutVariation.stacked,
+                      gridLayoutVariation:
+                          StartGroupGridLayoutVariation.initial,
+                      dimensionCalculator:
+                          StartGroupImplementation.startLinkDimensionCalculator,
+                      gapSize: defaultGapSize,
+                      itemBuilder: (context, item) {
+                        return LinkTile(
+                          title: item.$1(context),
+                          path: item.$2,
+                          icon: item.$3,
+                        );
+                      },
+                      direction: Axis.horizontal,
+                    ),
                 ],
               );
             },
