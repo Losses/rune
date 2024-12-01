9555ba352245e2c57624f51b9270ea5df186aa34
Shibo Lyu <hi@lao.sb>
2024-12-01 20:14:47 +0800

    fix(mac): Include team id and bundle id in entitlements.

--- scripts/macos_3_prepare_before_sign.sh
+++ scripts/macos_3_prepare_before_sign.sh
@@ -11,3 +11,8 @@ mkdir temp_macos
 ditto build/macos/Build/Products/Release/Rune.app temp_macos/Rune.app
 cp macos/Runner/Release.entitlements temp_macos
 cp ~/Library/MobileDevice/Provisioning\ Profiles/*.provisionprofile temp_macos/Rune.app/Contents/embedded.provisionprofile
+
+profile_name="$( ls -t ~/Library/MobileDevice/Provisioning\ Profiles/ | head -n1 )"
+if [[ "$profile_name" == *"App_Store"* ]]; then
+    sed -i '' 's/<string>LG57TUQ726.ci.not.rune<\/string>/<string>LG57TUQ726.ci.not.rune.appstore<\/string>/g' temp_macos/Release.entitlements
+fi
