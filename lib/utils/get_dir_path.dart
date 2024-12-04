22fb4daff8a63748b8fd41ba3170ab0449a3a7ac
XMLHexagram <52130356+XMLHexagram@users.noreply.github.com>
2024-12-04 01:40:35 +0100

    fix(macOS): Traffic light reset after showing file seletor (#168)

---
+++ lib/utils/get_dir_path.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+import 'package:file_selector/file_selector.dart';
+
+import 'macos_window_control_button_manager.dart';
+
+// TODO(hexagram): This is a temporary solution, not the best way to handle this callback.
+// Once BitsdojoWindow is forked and rewritten, using NSWindowDelagate to listen for windowDidEndSheet and windowWillBeginSheet will be a more general andbetter solution to handle this.
+Future<String?> getDirPath() async {
+  final path = await getDirectoryPath();
+  if (Platform.isMacOS) {
+    MacOSWindowControlButtonManager.setVertical();
+  }
+  return path;
+}
