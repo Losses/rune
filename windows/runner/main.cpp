99e71ad386b732d4ec3fd985edc2a5d368f69fa7
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-12-04 12:51:35 +0800

    feat: Add single instance support for Windows

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -10,6 +10,28 @@ auto bdw = bitsdojo_window_configure(BDW_CUSTOM_FRAME | BDW_HIDE_ON_STARTUP);
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
+  // Create a mutex
+  HANDLE hMutex = CreateMutex(NULL, TRUE, L"Rune_SingleInstance_Mutex");
+  DWORD dwError = GetLastError();
+  
+  // If the mutex already exists, it means another instance is running
+  if (dwError == ERROR_ALREADY_EXISTS) {
+    // Find the existing window
+    HWND hwnd = ::FindWindow(L"FLUTTER_RUNNER_WIN32_WINDOW", L"Rune");
+    if (hwnd != NULL) {
+      // If the window is minimized, restore it
+      if (::IsIconic(hwnd)) {
+        ::ShowWindow(hwnd, SW_RESTORE);
+      }
+      // Bring the window to the foreground
+      ::SetForegroundWindow(hwnd);
+    }
+    // Release the mutex handle
+    ReleaseMutex(hMutex);
+    CloseHandle(hMutex);
+    return EXIT_FAILURE;
+  }
+
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
@@ -42,5 +64,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   }
 
   ::CoUninitialize();
+
+  ReleaseMutex(hMutex);
+  CloseHandle(hMutex);
   return EXIT_SUCCESS;
 }
