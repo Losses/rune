a1c23df12427d43166c8987310b6807d175ed988
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-12-11 17:30:17 +0800

    feat: Implement force Zune mode config

--- lib/screens/settings_home/settings_home.dart
+++ lib/screens/settings_home/settings_home.dart
@@ -1,12 +1,17 @@
 import 'package:provider/provider.dart';
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
+import '../../utils/l10n.dart';
+import '../../utils/settings_manager.dart';
 import '../../widgets/belt_container.dart';
 import '../../widgets/navigation_bar/page_content_frame.dart';
 import '../../widgets/turntile/small_screen_feature_list.dart';
 import '../../widgets/band_screen/band_screen_feature_list.dart';
 import '../../widgets/start_screen/providers/start_screen_layout_manager.dart';
 import '../../providers/responsive_providers.dart';
+
+import '../settings_about/settings_about.dart';
 
 import 'constants/first_column.dart';
 import 'large_screen_settings_home_list.dart';
@@ -20,6 +25,21 @@ class SettingsHomePage extends StatefulWidget {
 
 class _SettingsHomePageState extends State<SettingsHomePage> {
   final _layoutManager = StartScreenLayoutManager();
+
+  bool? mysterious;
+
+  @override
+  void initState() {
+    super.initState();
+
+    SettingsManager().getValue<bool>(mysteriousKey).then((x) {
+      if (!context.mounted) return;
+
+      setState(() {
+        mysterious = x;
+      });
+    });
+  }
 
   @override
   void dispose() {
@@ -65,7 +85,16 @@ class _SettingsHomePageState extends State<SettingsHomePage> {
 
                 if (activeBreakpoint == DeviceType.zune) {
                   return SmallScreenFeatureListView(
-                    items: firstColumn,
+                    items: [
+                      ...firstColumn,
+                      if (mysterious == true)
+                        (
+                          (context) => S.of(context).laboratory,
+                          '/settings/laboratory',
+                          Symbols.interests,
+                          false
+                        )
+                    ],
                     layoutManager: _layoutManager,
                     topPadding: !isDock,
                   );
@@ -74,6 +103,7 @@ class _SettingsHomePageState extends State<SettingsHomePage> {
                 return LargeScreenSettingsHomeListView(
                   layoutManager: _layoutManager,
                   topPadding: !isDock,
+                  mysterious: mysterious,
                 );
               },
             );
