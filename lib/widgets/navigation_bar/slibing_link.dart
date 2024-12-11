b7ae70a28d411f4e04bfb6ae75aac6f4b295d050
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-12-11 09:20:28 +0800

    chore: Optimize tap region size

--- lib/widgets/navigation_bar/slibing_link.dart
+++ lib/widgets/navigation_bar/slibing_link.dart
@@ -96,19 +96,22 @@ class _SlibingLinkState extends State<SlibingLink> {
             key: Key('animation-$childFlipKey'),
             opacity: _entryAnimationOpacity,
             duration: const Duration(milliseconds: 300),
-            child: FlipText(
-              key: Key(childFlipKey),
-              flipKey: childFlipKey,
-              text: widget.route.titleBuilder(context),
-              scale: 1.2,
-              color: _isFocus ? contentColor : null,
-              glowColor: contentColor,
-              glowRadius: _isFocus ? 10 : 0,
-              alpha: widget.isSelected || _isFocus
-                  ? 255
-                  : _isHovered
-                      ? 200
-                      : 100,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 12),
+              child: FlipText(
+                key: Key(childFlipKey),
+                flipKey: childFlipKey,
+                text: widget.route.titleBuilder(context),
+                scale: 1.2,
+                color: _isFocus ? contentColor : null,
+                glowColor: contentColor,
+                glowRadius: _isFocus ? 10 : 0,
+                alpha: widget.isSelected || _isFocus
+                    ? 255
+                    : _isHovered
+                        ? 200
+                        : 100,
+              ),
             ),
           ),
         ),
