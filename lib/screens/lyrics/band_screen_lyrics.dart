029b955b35b71cf4a96ef7956b3352ae6518a061
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-12-15 16:57:26 +0800

    feat: Lyrics view supports the dock view now

--- lib/screens/lyrics/band_screen_lyrics.dart
+++ lib/screens/lyrics/band_screen_lyrics.dart
@@ -1,6 +1,9 @@
+import 'package:provider/provider.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 
 import '../../messages/lyric.pb.dart';
+import '../../providers/responsive_providers.dart';
+
 import 'widgets/lyric_display.dart';
 
 class BandScreenLyricsView extends StatefulWidget {
@@ -24,6 +27,20 @@ class BandScreenLyricsView extends StatefulWidget {
 class LibraryHomeListState extends State<BandScreenLyricsView> {
   @override
   Widget build(BuildContext context) {
+    final r = Provider.of<ResponsiveProvider>(context);
+    final isMini = r.smallerOrEqualTo(DeviceType.dock, false);
+
+    if (isMini) {
+      return RotatedBox(
+        quarterTurns: 1,
+        child: LyricsDisplay(
+          lyrics: widget.lyrics,
+          currentTimeMilliseconds: widget.currentTimeMilliseconds,
+          activeLines: widget.activeLines,
+        ),
+      );
+    }
+
     return LyricsDisplay(
       lyrics: widget.lyrics,
       currentTimeMilliseconds: widget.currentTimeMilliseconds,
