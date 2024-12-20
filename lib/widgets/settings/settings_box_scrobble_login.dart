5ef934983933edc3c717ffa408b1c4aa5573b14f
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-12-20 12:37:13 +0800

    feat: Add logout confirmation dialog and error options menu

--- lib/widgets/settings/settings_box_scrobble_login.dart
+++ lib/widgets/settings/settings_box_scrobble_login.dart
@@ -2,6 +2,7 @@ import 'package:provider/provider.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:collection/collection.dart';
 
+import '../../utils/dialogs/information/information.dart';
 import '../../utils/l10n.dart';
 import '../../utils/dialogs/scrobble/show_scrobble_login_dialog.dart';
 import '../../providers/scrobble.dart';
@@ -39,15 +40,68 @@ class SettingsBoxScrobbleLogin extends SettingsBoxBase {
 
     return Button(
       onPressed: isLoggedIn
-          ? null
-          : () => showScrobbleLoginDialog(
-                context,
-                serviceName,
-                title,
-              ),
+          ? () => _showLogoutConfirmation(context)
+          : () {
+              if (hasError) {
+                _showErrorOptionsMenu(context);
+              } else {
+                showScrobbleLoginDialog(context, serviceName, title);
+              }
+            },
       child: Text(
         isLoggedIn ? s.edit : (hasError ? s.fix : s.login),
       ),
+    );
+  }
+
+  void _showLogoutConfirmation(BuildContext context) {
+    final s = S.of(context);
+    showInformationDialog(
+      context: context,
+      title: s.confirmLogoutTitle,
+      subtitle: s.confirmLogoutSubtitle,
+    ).then((confirmed) {
+      if (confirmed == true) {
+        if (!context.mounted) return;
+
+        Provider.of<ScrobbleProvider>(context, listen: false)
+            .logout(serviceName);
+      }
+    });
+  }
+
+  void _showErrorOptionsMenu(BuildContext context) {
+    final s = S.of(context);
+    final menuController = FlyoutController();
+
+    menuController.showFlyout(
+      builder: (context) {
+        return MenuFlyout(
+          items: [
+            MenuFlyoutItem(
+              text: Text(s.retryLogin),
+              onPressed: () {
+                Navigator.pop(context);
+                showScrobbleLoginDialog(context, serviceName, title);
+              },
+            ),
+            MenuFlyoutItem(
+              text: Text(s.logout),
+              onPressed: () {
+                Navigator.pop(context);
+                _showLogoutConfirmation(context);
+              },
+            ),
+            MenuFlyoutItem(
+              text: Text(s.edit),
+              onPressed: () {
+                Navigator.pop(context);
+                showScrobbleLoginDialog(context, serviceName, title);
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
