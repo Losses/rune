14804226cec8888b708cc850d10282c795cf4692
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-12-20 09:31:44 +0800

    feat: Tweak action button for failed login settings item

--- lib/widgets/settings/settings_box_scrobble_login.dart
+++ lib/widgets/settings/settings_box_scrobble_login.dart
@@ -1,7 +1,10 @@
+import 'package:provider/provider.dart';
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:collection/collection.dart';
 
 import '../../utils/l10n.dart';
 import '../../utils/dialogs/scrobble/show_scrobble_login_dialog.dart';
+import '../../providers/scrobble.dart';
 
 import 'settings_box_base.dart';
 
@@ -26,13 +29,24 @@ class SettingsBoxScrobbleLogin extends SettingsBoxBase {
   @override
   Widget buildDefaultContent(BuildContext context) {
     final s = S.of(context);
+    final scrobbleProvider = Provider.of<ScrobbleProvider>(context);
+
+    final serviceStatus = scrobbleProvider.serviceStatuses
+        .firstWhereOrNull((status) => status.serviceId == serviceName);
+
+    bool isLoggedIn = serviceStatus != null && serviceStatus.isAvailable;
+    bool hasError = serviceStatus != null && serviceStatus.error.isNotEmpty;
 
     return Button(
-      child: Text(s.login),
-      onPressed: () => showScrobbleLoginDialog(
-        context,
-        serviceName,
-        title,
+      onPressed: isLoggedIn
+          ? null
+          : () => showScrobbleLoginDialog(
+                context,
+                serviceName,
+                title,
+              ),
+      child: Text(
+        isLoggedIn ? s.edit : (hasError ? s.fix : s.login),
       ),
     );
   }
