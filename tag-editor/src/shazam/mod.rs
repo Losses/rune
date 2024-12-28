f9316797d2d8aa9ca47a75ecc1937263deece2d4
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-12-28 14:57:52 +0800

    docs(shazam): Add licensing information and project attribution

--- tag-editor/src/shazam/mod.rs
+++ tag-editor/src/shazam/mod.rs
@@ -1,2 +1,10 @@
+/**
+ * This directory contains programs based on the implementation of cgzirim/seek-tune. 
+ * The original project's source code is licensed under the MIT License and can be
+ * found on GitHub. 
+ * 
+ * This Rust-based refactored version in this repository is licensed under the
+ * MPL License.
+ */
 pub mod spectrogram;
 mod filter;
