5923beaff276807042e50ecc64cc894330ac68c6
LOSSES Don <1384036+Losses@users.noreply.github.com>
2025-01-13 19:21:39 +0800

    feat(server): Add macros for forwarding events to remote handlers

--- native/hub/src/server_request.rs
+++ native/hub/src/server_request.rs
@@ -27,3 +27,38 @@ macro_rules! implement_rinf_dart_signal_trait {
         )*
     };
 }
+
+#[macro_export]
+macro_rules! forward_event_to_remote {
+    ($bridge:expr) => {
+        process_forward_event_to_remote_handlers!(@internal $bridge);
+    };
+}
+
+#[macro_export]
+macro_rules! process_forward_event_to_remote_handlers {
+    (@internal $bridge:expr, $response:ty) => {
+        paste::paste! {
+            handle_single_to_remote_event!($bridge, $response).await;
+        }
+    };
+    (@internal $bridge:expr $(,)?) => {};
+}
+
+#[macro_export]
+macro_rules! handle_single_to_remote_event {
+    (@internal $bridge:expr, $response:ty) => {
+        paste::paste! {
+            paste::paste! {
+                async fn [<handle_event_ $request:snake>](global_params: Arc<GlobalParams>) {
+                    let receiver = <$request>::get_dart_signal_receiver();
+                    while let Some(dart_signal) = receiver.recv().await {
+                        bridge.send_message(dart_signal);
+                    }
+                }
+                tokio::spawn([<handle_event_ $request:snake>]($global_params.clone()));
+            }
+        }
+    };
+    (@internal $bridge:expr $(,)?) => {};
+}
