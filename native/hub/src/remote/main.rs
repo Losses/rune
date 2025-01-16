89662179284026d6d8eb7625b4ca31d0763758ec
LOSSES Don <1384036+Losses@users.noreply.github.com>
2025-01-16 11:34:06 +0800

    feat(remote): Enhance DIYHinter with filename completion and bracket validation

--- native/hub/src/remote/main.rs
+++ native/hub/src/remote/main.rs
@@ -1,14 +1,25 @@
+use std::borrow::Cow::{self, Borrowed, Owned};
+
 use radix_trie::{Trie, TrieCommon};
-use rustyline::hint::{Hint, Hinter};
+use rustyline::completion::FilenameCompleter;
+use rustyline::error::ReadlineError;
+use rustyline::highlight::{CmdKind, Highlighter, MatchingBracketHighlighter};
+use rustyline::hint::Hint;
 use rustyline::history::DefaultHistory;
-use rustyline::Context;
-use rustyline::{Editor, Result};
-use rustyline_derive::{Completer, Helper, Highlighter, Validator};
+use rustyline::validate::MatchingBracketValidator;
+use rustyline::{Cmd, CompletionType, Config, Context, EditMode, Editor, KeyEvent, Result};
+use rustyline_derive::{Completer, Helper, Validator};
 use tracing_subscriber::EnvFilter;
 
-#[derive(Completer, Helper, Validator, Highlighter)]
+#[derive(Helper, Completer, Validator)]
 struct DIYHinter {
+    #[rustyline(Completer)]
+    completer: FilenameCompleter,
+    highlighter: MatchingBracketHighlighter,
+    #[rustyline(Validator)]
+    validator: MatchingBracketValidator,
     hints: Trie<String, CommandHint>,
+    colored_prompt: String,
 }
 
 #[derive(Hash, Debug, PartialEq, Eq, Clone)]
@@ -48,7 +59,7 @@ impl CommandHint {
     }
 }
 
-impl Hinter for DIYHinter {
+impl rustyline::hint::Hinter for DIYHinter {
     type Hint = CommandHint;
 
     fn hint(&self, line: &str, pos: usize, _ctx: &Context<'_>) -> Option<CommandHint> {
@@ -60,6 +71,32 @@ impl Hinter for DIYHinter {
             .get_raw_descendant(line)
             .and_then(|node| node.value())
             .map(|hint| hint.suffix(pos))
+    }
+}
+
+impl Highlighter for DIYHinter {
+    fn highlight_prompt<'b, 's: 'b, 'p: 'b>(
+        &'s self,
+        prompt: &'p str,
+        default: bool,
+    ) -> Cow<'b, str> {
+        if default {
+            Borrowed(&self.colored_prompt)
+        } else {
+            Borrowed(prompt)
+        }
+    }
+
+    fn highlight_hint<'h>(&self, hint: &'h str) -> Cow<'h, str> {
+        Owned("\x1b[1m".to_owned() + hint + "\x1b[m")
+    }
+
+    fn highlight<'l>(&self, line: &'l str, pos: usize) -> Cow<'l, str> {
+        self.highlighter.highlight(line, pos)
+    }
+
+    fn highlight_char(&self, line: &str, pos: usize, kind: CmdKind) -> bool {
+        self.highlighter.highlight_char(line, pos, kind)
     }
 }
 
@@ -89,14 +126,56 @@ fn main() -> Result<()> {
         .with_test_writer()
         .init();
 
-    println!("Welcome to the Rune Speaker Command Line Interface");
-    let h = DIYHinter { hints: diy_hints() };
+    let config = Config::builder()
+        .history_ignore_space(true)
+        .completion_type(CompletionType::List)
+        .edit_mode(EditMode::Emacs)
+        .build();
 
-    let mut rl: Editor<DIYHinter, DefaultHistory> = Editor::new()?;
+    let h = DIYHinter {
+        completer: FilenameCompleter::new(),
+        highlighter: MatchingBracketHighlighter::new(),
+        hints: diy_hints(),
+        colored_prompt: "".to_owned(),
+        validator: MatchingBracketValidator::new(),
+    };
+
+    let mut rl: Editor<DIYHinter, DefaultHistory> = Editor::with_config(config)?;
     rl.set_helper(Some(h));
+    rl.bind_sequence(KeyEvent::alt('n'), Cmd::HistorySearchForward);
+    rl.bind_sequence(KeyEvent::alt('p'), Cmd::HistorySearchBackward);
 
-    loop {
-        let input = rl.readline("> ")?;
-        println!("input: {input}");
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
     }
+
+    println!("Welcome to the Rune Speaker Command Line Interface");
+
+    let mut count = 1;
+    loop {
+        let p = format!("{count}> ");
+        rl.helper_mut().expect("No helper").colored_prompt = format!("\x1b[1;32m{p}\x1b[0m");
+        let readline = rl.readline(&p);
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str())?;
+                println!("input: {line}");
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("Interrupted");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("Encountered Eof");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {err:?}");
+                break;
+            }
+        }
+        count += 1;
+    }
+
+    rl.append_history("history.txt")
 }
