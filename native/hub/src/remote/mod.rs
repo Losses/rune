d80862702f2c5911b45f3bef116eb762b1fd6738
LOSSES Don <1384036+Losses@users.noreply.github.com>
2025-01-21 15:26:26 +0800

    chore(remote): Change log level from info to debug for received message type

--- native/hub/src/remote/mod.rs
+++ native/hub/src/remote/mod.rs
@@ -6,6 +6,7 @@ use std::{collections::HashMap, sync::Arc};
 use anyhow::Result;
 use futures::SinkExt;
 use futures::StreamExt;
+use log::debug;
 use log::{error, info};
 use prost::Message as ProstMessage;
 use tokio::sync::Mutex;
@@ -201,7 +202,7 @@ impl WebSocketDartBridge {
                                     Some(Ok(msg)) => {
                                         if let TungsteniteMessage::Binary(payload) = msg {
                                             if let Some((msg_type, msg_payload, _request_id)) = decode_message(&payload) {
-                                                info!("Received message with type: {}", msg_type);
+                                                debug!("Received message with type: {}", msg_type);
                                                 if let Some(handler) = handlers.lock().await.get(&msg_type) {
                                                     handler(msg_payload);
                                                 } else {
