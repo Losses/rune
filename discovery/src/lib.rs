59182b23c8e3229a34acdf716b33470bce20265b
LOSSES Don <1384036+Losses@users.noreply.github.com>
2025-02-25 11:59:24 +0800

    feat(url): add encoding and decoding functions for RuneScape server URLs

--- discovery/src/lib.rs
+++ discovery/src/lib.rs
@@ -1,12 +1,13 @@
 use utils::DeviceInfo;
 
-pub mod server;
-pub mod persistent;
-pub mod request;
-pub mod ssl;
-pub mod protocol;
-pub mod utils;
 pub mod client;
+pub mod persistent;
+pub mod protocol;
+pub mod request;
+pub mod server;
+pub mod ssl;
+pub mod url;
+pub mod utils;
 
 pub struct DiscoveryParams {
     pub device_info: DeviceInfo,
