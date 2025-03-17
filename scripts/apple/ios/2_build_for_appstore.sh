3f9b1205fd25a2b66f91dbb18ad55aa310746012
Shibo Lyu <hi@lao.sb>
2025-03-17 19:19:00 +0800

    fix(ci): ios entitlement path

--- scripts/apple/ios/2_build_for_appstore.sh
+++ scripts/apple/ios/2_build_for_appstore.sh
@@ -8,7 +8,7 @@ cd ../../..
 # Adjust build settings to use correct bundle ID, provisioning profile, entitlements, and signing identity.
 cp ios/Runner.xcodeproj/project.pbxproj ios/Runner.xcodeproj/project.pbxproj.backup
 sed -i '' 's/PRODUCT_BUNDLE_IDENTIFIER = ci.not.rune;/PRODUCT_BUNDLE_IDENTIFIER = ci.not.rune.appstore;/g' ios/Runner.xcodeproj/project.pbxproj
-sed -i '' 's/<string>LG57TUQ726.ci.not.rune<\/string>/<string>LG57TUQ726.ci.not.rune.appstore<\/string>/g' ios/Runner/Release.entitlements
+sed -i '' 's/<string>LG57TUQ726.ci.not.rune<\/string>/<string>LG57TUQ726.ci.not.rune.appstore<\/string>/g' ios/Runner/Runner.entitlements
 sed -i '' 's/"PROVISIONING_PROFILE_SPECIFIER\[sdk=iphoneos\*]" = "Rune";/"PROVISIONING_PROFILE_SPECIFIER[sdk=iphoneos*]" = "Rune iOS App Store";/g' ios/Runner.xcodeproj/project.pbxproj
 
 flutter pub get
