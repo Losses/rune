4e9224e43fb14a43660e43b068f7224dec359c1f
LOSSES Don <1384036+Losses@users.noreply.github.com>
2025-05-23 16:59:00 +0800

    fix: Incorrect migration script

--- migration/src/m20250410_000025_add_hlc_columns.rs
+++ migration/src/m20250410_000025_add_hlc_columns.rs
@@ -32,49 +32,197 @@ impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         println!("Applying migration: Add tracking columns");
 
-        Self::add_all_tracking_columns(manager, Albums::Table).await?;
-        Self::add_all_tracking_columns(manager, Artists::Table).await?;
-        Self::add_all_tracking_columns(manager, Genres::Table).await?;
-        Self::add_all_tracking_columns(manager, MediaAnalysis::Table).await?;
-        Self::add_all_tracking_columns(manager, MediaCoverArt::Table).await?;
-        Self::add_all_tracking_columns(manager, MediaFiles::Table).await?;
-        Self::add_all_tracking_columns(manager, MediaMetadata::Table).await?;
-        Self::add_all_tracking_columns(manager, MediaFilePlaylists::Table).await?;
-        Self::add_all_tracking_columns(manager, Mixes::Table).await?;
-        Self::add_all_tracking_columns(manager, MixQueries::Table).await?;
-        Self::add_all_tracking_columns(manager, Playlists::Table).await?;
+        Self::add_tracking_columns(manager, Albums::Table, true).await?;
+        Self::add_tracking_columns(manager, Artists::Table, true).await?;
+        Self::add_tracking_columns(manager, Genres::Table, true).await?;
+        Self::add_tracking_columns(manager, MediaAnalysis::Table, true).await?;
+        Self::add_tracking_columns(manager, MediaCoverArt::Table, true).await?;
+        Self::add_tracking_columns(manager, MediaFiles::Table, true).await?;
+        Self::add_tracking_columns(manager, MediaMetadata::Table, true).await?;
+        Self::add_tracking_columns(manager, MediaFilePlaylists::Table, true).await?;
+
+        Self::add_tracking_columns_with_existing_ts(manager, Mixes::Table).await?;
+        Self::add_tracking_columns_with_existing_ts(manager, MixQueries::Table).await?;
+        Self::add_tracking_columns_with_existing_ts(manager, Playlists::Table).await?;
+
         Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        Self::remove_all_tracking_columns(manager, Albums::Table).await?;
-        Self::remove_all_tracking_columns(manager, Artists::Table).await?;
-        Self::remove_all_tracking_columns(manager, Genres::Table).await?;
-        Self::remove_all_tracking_columns(manager, MediaAnalysis::Table).await?;
-        Self::remove_all_tracking_columns(manager, MediaCoverArt::Table).await?;
-        Self::remove_all_tracking_columns(manager, MediaFiles::Table).await?;
-        Self::remove_all_tracking_columns(manager, MediaMetadata::Table).await?;
-        Self::remove_all_tracking_columns(manager, Mixes::Table).await?;
-        Self::remove_all_tracking_columns(manager, MixQueries::Table).await?;
-        Self::remove_all_tracking_columns(manager, Playlists::Table).await?;
+        Self::remove_tracking_columns(manager, Albums::Table, true).await?;
+        Self::remove_tracking_columns(manager, Artists::Table, true).await?;
+        Self::remove_tracking_columns(manager, Genres::Table, true).await?;
+        Self::remove_tracking_columns(manager, MediaAnalysis::Table, true).await?;
+        Self::remove_tracking_columns(manager, MediaCoverArt::Table, true).await?;
+        Self::remove_tracking_columns(manager, MediaFiles::Table, true).await?;
+        Self::remove_tracking_columns(manager, MediaMetadata::Table, true).await?;
+        Self::remove_tracking_columns(manager, MediaFilePlaylists::Table, true).await?;
+
+        Self::remove_tracking_columns_with_existing_ts(manager, Mixes::Table).await?;
+        Self::remove_tracking_columns_with_existing_ts(manager, MixQueries::Table).await?;
+        Self::remove_tracking_columns_with_existing_ts(manager, Playlists::Table).await?;
 
         Ok(())
     }
 }
 
 impl Migration {
-    async fn add_all_tracking_columns<'a, T>(
+    async fn add_tracking_columns<'a, T>(
+        manager: &'a SchemaManager<'a>,
+        table: T,
+        include_ts: bool,
+    ) -> Result<(), DbErr>
+    where
+        T: Iden + Copy + 'static,
+    {
+        let default_timestamp_value =
+            Value::String(Some(Box::new("1970-01-01 00:00:00.000".to_string())));
+
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(table)
+                    .add_column(
+                        ColumnDef::new(CommonColumns::HlcUuid)
+                            .string()
+                            .not_null()
+                            .default(""),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        if include_ts {
+            manager
+                .alter_table(
+                    Table::alter()
+                        .table(table)
+                        .add_column(
+                            ColumnDef::new(CommonColumns::CreatedAtHlcTs)
+                                .timestamp()
+                                .not_null()
+                                .default(default_timestamp_value.clone()),
+                        )
+                        .to_owned(),
+                )
+                .await?;
+
+            manager
+                .alter_table(
+                    Table::alter()
+                        .table(table)
+                        .add_column(
+                            ColumnDef::new(CommonColumns::UpdatedAtHlcTs)
+                                .timestamp()
+                                .not_null()
+                                .default(default_timestamp_value),
+                        )
+                        .to_owned(),
+                )
+                .await?;
+        }
+
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(table)
+                    .add_column(
+                        ColumnDef::new(CommonColumns::CreatedAtHlcVer)
+                            .integer()
+                            .not_null()
+                            .default(0),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(table)
+                    .add_column(
+                        ColumnDef::new(CommonColumns::CreatedAtHlcNid)
+                            .text()
+                            .not_null()
+                            .default(""),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(table)
+                    .add_column(
+                        ColumnDef::new(CommonColumns::UpdatedAtHlcVer)
+                            .integer()
+                            .not_null()
+                            .default(0),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(table)
+                    .add_column(
+                        ColumnDef::new(CommonColumns::UpdatedAtHlcNid)
+                            .text()
+                            .not_null()
+                            .default(""),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
+    }
+
+    async fn add_tracking_columns_with_existing_ts<'a, T>(
         manager: &'a SchemaManager<'a>,
         table: T,
     ) -> Result<(), DbErr>
     where
         T: Iden + Copy + 'static,
     {
-        // A common placeholder timestamp string.
-        // SQLite will store this as TEXT.
-        // Ensure your application logic correctly sets these timestamps on new/updated records.
-        let default_timestamp_value =
-            Value::String(Some(Box::new("1970-01-01 00:00:00.000".to_string())));
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(table)
+                    .add_column(
+                        ColumnDef::new(CommonColumns::CreatedAtHlcTs)
+                            .timestamp()
+                            .not_null(),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(table)
+                    .add_column(
+                        ColumnDef::new(CommonColumns::UpdatedAtHlcTs)
+                            .timestamp()
+                            .not_null(),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .exec_stmt(
+                Query::update()
+                    .table(table)
+                    .value(CommonColumns::CreatedAtHlcTs, Expr::col(Mixes::CreatedAt))
+                    .value(CommonColumns::UpdatedAtHlcTs, Expr::col(Mixes::UpdatedAt))
+                    .to_owned(),
+            )
+            .await?;
 
         manager
             .alter_table(
@@ -95,10 +243,24 @@ impl Migration {
                 Table::alter()
                     .table(table)
                     .add_column(
-                        ColumnDef::new(CommonColumns::UpdatedAtHlcTs)
-                            .timestamp()
+                        ColumnDef::new(CommonColumns::CreatedAtHlcVer)
+                            .integer()
                             .not_null()
-                            .default(default_timestamp_value.clone()),
+                            .default(0),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(table)
+                    .add_column(
+                        ColumnDef::new(CommonColumns::CreatedAtHlcNid)
+                            .text()
+                            .not_null()
+                            .default(""),
                     )
                     .to_owned(),
             )
@@ -136,12 +298,7 @@ impl Migration {
             .alter_table(
                 Table::alter()
                     .table(table)
-                    .add_column(
-                        ColumnDef::new(CommonColumns::CreatedAtHlcTs)
-                            .timestamp()
-                            .not_null()
-                            .default(default_timestamp_value),
-                    )
+                    .drop_column(Mixes::CreatedAt)
                     .to_owned(),
             )
             .await?;
@@ -150,26 +307,7 @@ impl Migration {
             .alter_table(
                 Table::alter()
                     .table(table)
-                    .add_column(
-                        ColumnDef::new(CommonColumns::CreatedAtHlcVer)
-                            .integer()
-                            .not_null()
-                            .default(0),
-                    )
-                    .to_owned(),
-            )
-            .await?;
-
-        manager
-            .alter_table(
-                Table::alter()
-                    .table(table)
-                    .add_column(
-                        ColumnDef::new(CommonColumns::CreatedAtHlcNid)
-                            .text()
-                            .not_null()
-                            .default(""),
-                    )
+                    .drop_column(Mixes::UpdatedAt)
                     .to_owned(),
             )
             .await?;
@@ -177,7 +315,83 @@ impl Migration {
         Ok(())
     }
 
-    async fn remove_all_tracking_columns<'a, T>(
+    async fn remove_tracking_columns<'a, T>(
+        manager: &'a SchemaManager<'a>,
+        table: T,
+        include_ts: bool,
+    ) -> Result<(), DbErr>
+    where
+        T: Iden + Copy + 'static,
+    {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(table)
+                    .drop_column(CommonColumns::HlcUuid)
+                    .to_owned(),
+            )
+            .await?;
+
+        if include_ts {
+            manager
+                .alter_table(
+                    Table::alter()
+                        .table(table)
+                        .drop_column(CommonColumns::CreatedAtHlcTs)
+                        .to_owned(),
+                )
+                .await?;
+
+            manager
+                .alter_table(
+                    Table::alter()
+                        .table(table)
+                        .drop_column(CommonColumns::UpdatedAtHlcTs)
+                        .to_owned(),
+                )
+                .await?;
+        }
+
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(table)
+                    .drop_column(CommonColumns::CreatedAtHlcVer)
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(table)
+                    .drop_column(CommonColumns::CreatedAtHlcNid)
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(table)
+                    .drop_column(CommonColumns::UpdatedAtHlcVer)
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(table)
+                    .drop_column(CommonColumns::UpdatedAtHlcNid)
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
+    }
+
+    async fn remove_tracking_columns_with_existing_ts<'a, T>(
         manager: &'a SchemaManager<'a>,
         table: T,
     ) -> Result<(), DbErr>
@@ -206,6 +420,15 @@ impl Migration {
             .alter_table(
                 Table::alter()
                     .table(table)
+                    .drop_column(CommonColumns::UpdatedAtHlcTs)
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(table)
                     .drop_column(CommonColumns::CreatedAtHlcVer)
                     .to_owned(),
             )
@@ -224,15 +447,6 @@ impl Migration {
             .alter_table(
                 Table::alter()
                     .table(table)
-                    .drop_column(CommonColumns::UpdatedAtHlcTs)
-                    .to_owned(),
-            )
-            .await?;
-
-        manager
-            .alter_table(
-                Table::alter()
-                    .table(table)
                     .drop_column(CommonColumns::UpdatedAtHlcVer)
                     .to_owned(),
             )
@@ -243,6 +457,24 @@ impl Migration {
                 Table::alter()
                     .table(table)
                     .drop_column(CommonColumns::UpdatedAtHlcNid)
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(table)
+                    .add_column(ColumnDef::new(Mixes::CreatedAt).timestamp().not_null())
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(table)
+                    .add_column(ColumnDef::new(Mixes::UpdatedAt).timestamp().not_null())
                     .to_owned(),
             )
             .await?;
