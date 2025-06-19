c0ad1c8ccd1bb2c7c27a3d661e0c4c5ecf6509d8
LOSSES Don <1384036+Losses@users.noreply.github.com>
2025-06-19 16:31:57 +0800

    fix(sync): Remove incorrect data convertion method

--- database/src/sync/utils.rs
+++ database/src/sync/utils.rs
@@ -1,21 +1,10 @@
 use anyhow::Result;
-use chrono::{DateTime, FixedOffset, TimeZone, Utc};
+use chrono::{DateTime, FixedOffset};
 use uuid::Uuid;
 
 use ::sync::hlc::HLC;
 
 use crate::entities::sync_record::ActiveModel;
-
-fn convert_to_rfc3339(timestamp_ms: u64) -> Result<String> {
-    let secs = (timestamp_ms / 1000) as i64;
-    let nanos = ((timestamp_ms % 1000) * 1_000_000) as u32;
-
-    Ok(Utc
-        .timestamp_opt(secs, nanos)
-        .single()
-        .ok_or_else(|| anyhow::anyhow!("invalid timestamp"))?
-        .to_rfc3339())
-}
 
 pub fn create_sync_record_active_model(
     table_name: String,
@@ -26,7 +15,7 @@ pub fn create_sync_record_active_model(
     Ok(ActiveModel {
         table_name: Set(table_name),
         client_node_id: Set(client_node_id.to_string()),
-        last_sync_hlc_ts: Set(convert_to_rfc3339(hlc.timestamp)?),
+        last_sync_hlc_ts: Set(hlc.to_rfc3339()),
         last_sync_hlc_ver: Set(hlc.version as i32),
         last_sync_hlc_nid: Set(hlc.node_id.to_string()),
         ..Default::default()
