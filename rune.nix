6430ece63c267614a247678c0491f87bb543b9cf
LOSSES Don <1384036+Losses@users.noreply.github.com>
2025-09-02 18:06:59 -0400

    chore: Bump to `v2.0.0-alpha.8

--- rune.nix
+++ rune.nix
@@ -15,7 +15,7 @@
 }:
 
 let
-  version = "2.0.1007";
+  version = "2.0.1008";
 
   metaCommon = {
     description = "Experience timeless melodies with a music player that blends classic design with modern technology";
