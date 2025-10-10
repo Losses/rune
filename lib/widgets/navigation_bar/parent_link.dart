d7331fef7233ee69c96bd8d553e835fa5b4688eb
LOSSES Don <1384036+Losses@users.noreply.github.com>
2025-10-10 19:21:06 -0400

    chore: Performance improvement

--- lib/widgets/navigation_bar/parent_link.dart
+++ lib/widgets/navigation_bar/parent_link.dart
@@ -22,6 +22,8 @@ class ParentLink extends StatefulWidget {
 class ParentLinkState extends State<ParentLink> {
   double _alpha = 80;
   bool _isFocus = false;
+  double? _cachedScaledWidth;
+  String? _cachedText;
 
   final FocusNode _focusNode = FocusNode(debugLabel: 'Parent Link');
 
@@ -43,6 +45,27 @@ class ParentLinkState extends State<ParentLink> {
     });
   }
 
+  double _getScaledWidth() {
+    if (_cachedScaledWidth == null || _cachedText != widget.text) {
+      final textPainter = TextPainter(
+        text: TextSpan(
+          text: widget.text,
+          style: const TextStyle(
+            fontSize: 17,
+            fontVariations: <FontVariation>[FontVariation('wght', 400)],
+          ),
+        ),
+        textDirection: TextDirection.ltr,
+      );
+      textPainter.layout();
+
+      _cachedScaledWidth = (textPainter.width * 5) + 12;
+      _cachedText = widget.text;
+    }
+
+    return _cachedScaledWidth!;
+  }
+
   void _onPressed() {
     if (comboBoxOpened) {
       return;
@@ -55,51 +78,31 @@ class ParentLinkState extends State<ParentLink> {
   Widget build(BuildContext context) {
     final accentColor = FluentTheme.of(context).accentColor;
 
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        final textPainter = TextPainter(
-          text: TextSpan(
-            text: widget.text,
-            style: const TextStyle(
-              fontSize: 17,
-              fontVariations: <FontVariation>[FontVariation('wght', 400)],
+    return Container(
+      constraints: BoxConstraints.tightFor(width: _getScaledWidth()),
+      padding: const EdgeInsets.only(right: 12),
+      child: Listener(
+        onPointerUp: (_) => _onPressed(),
+        child: FocusableActionDetector(
+          focusNode: _focusNode,
+          onShowFocusHighlight: _handleFocusHighlight,
+          onShowHoverHighlight: _handleHoverHighlight,
+          actions: {ActivateIntent: ActivateLinkAction(context, _onPressed)},
+          child: SizedBox(
+            height: 80,
+            child: FlipText(
+              key: Key(widget.titleFlipKey),
+              flipKey: widget.titleFlipKey,
+              text: widget.text,
+              scale: 5,
+              alpha: _isFocus ? 255 : _alpha,
+              color: _isFocus ? accentColor : null,
+              glowColor: accentColor,
+              glowRadius: _isFocus ? 10 : 0,
             ),
           ),
-          textDirection: TextDirection.ltr,
-        );
-        textPainter.layout();
-
-        final scaledWidth = (textPainter.width * 5) + 12;
-
-        return Container(
-          constraints: BoxConstraints.tightFor(width: scaledWidth),
-          padding: const EdgeInsets.only(right: 12),
-          child: Listener(
-            onPointerUp: (_) => _onPressed(),
-            child: FocusableActionDetector(
-              focusNode: _focusNode,
-              onShowFocusHighlight: _handleFocusHighlight,
-              onShowHoverHighlight: _handleHoverHighlight,
-              actions: {
-                ActivateIntent: ActivateLinkAction(context, _onPressed),
-              },
-              child: SizedBox(
-                height: 80,
-                child: FlipText(
-                  key: Key(widget.titleFlipKey),
-                  flipKey: widget.titleFlipKey,
-                  text: widget.text,
-                  scale: 5,
-                  alpha: _isFocus ? 255 : _alpha,
-                  color: _isFocus ? accentColor : null,
-                  glowColor: accentColor,
-                  glowRadius: _isFocus ? 10 : 0,
-                ),
-              ),
-            ),
-          ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
